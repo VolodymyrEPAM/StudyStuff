4ce5af769d4e9cbd79d666dc5013da2b7b7d5aaa
Volodymyr Tybolchuk <Volodymyr_Tybolchuk@epam.com>
2021-12-01 23:54:20 +0200

    FINALLY

--- src/main/java/SomeTaskThatNotActuallyHomework.java
+++ src/main/java/SomeTaskThatNotActuallyHomework.java
@@ -2,23 +2,18 @@ import java.util.Arrays;
 
 public class SomeTaskThatNotActuallyHomework {
     public static void main(String[] args) {
-        int[] array = new int[]{4, 2, 1, 3, 5};
-        bubbleSort(array);
+        int[] array = new int[]{5,57,1,2,34,3,5,8,76,32};
+        bbubbleSort(array);
     }
 
-    private static void bubbleSort(int[] array) {
+    private static void bbubbleSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                int x = array[i + 1];
-                array[i + 1] = array[i];
-                array[i] = x;
-            }
-        }
-        for (int j = 0; j < array.length - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                int y = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = y;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i]>array[j]){
+                    int x = array[j];
+                    array[j]=array[i];
+                    array[i]=x;
+                }
             }
         }
         System.out.println(Arrays.toString(array));
