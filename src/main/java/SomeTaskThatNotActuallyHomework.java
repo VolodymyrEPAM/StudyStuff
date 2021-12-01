550a4000347babc07ea5871eb1eb39c01df7e82f
Volodymyr Tybolchuk <Volodymyr_Tybolchuk@epam.com>
2021-12-01 21:51:33 +0200

    For Java Basic

--- src/main/java/SomeTaskThatNotActuallyHomework.java
+++ src/main/java/SomeTaskThatNotActuallyHomework.java
@@ -7,11 +7,18 @@ public class SomeTaskThatNotActuallyHomework {
     }
 
     private static void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length-1; i++) {
-            if (array[i] > array[i+1]) {
-             int x = array[i];
-                array[i] = array[i+1];
-                array[i+1] = x;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                int x = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = x;
+            }
+        }
+        for (int j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int y = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = y;
             }
         }
         System.out.println(Arrays.toString(array));
