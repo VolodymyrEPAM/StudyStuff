0ae48d26cbe82144c7f37944f8fb58ebce3496ca
Volodymyr Tybolchuk <Volodymyr_Tybolchuk@epam.com>
2021-12-12 15:46:44 +0200

    Homework is done

---
+++ src/main/java/JavaBasicPart2/Task4/NumberHoldrer.java
@@ -0,0 +1,19 @@
+package JavaBasicPart2.Task4;
+
+public class NumberHoldrer {
+    public int anInt;
+    public float aFloat;
+
+    public NumberHoldrer(int anInt, float aFloat) {
+        this.anInt = anInt;
+        this.aFloat = aFloat;
+    }
+
+    public int getAnInt() {
+        return anInt;
+    }
+
+    public float getaFloat() {
+        return aFloat;
+    }
+}
