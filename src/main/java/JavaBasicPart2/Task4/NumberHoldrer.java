fdcc5f46a61ad941768ccb70504cda43ffea414d
Volodymyr Tybolchuk <Volodymyr_Tybolchuk@epam.com>
2021-12-12 16:04:31 +0200

    Tasks fixed due to Lombok

--- src/main/java/JavaBasicPart2/Task4/NumberHoldrer.java
+++ src/main/java/JavaBasicPart2/Task4/NumberHoldrer.java
@@ -1,19 +1,11 @@
 package JavaBasicPart2.Task4;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class NumberHoldrer {
     public int anInt;
     public float aFloat;
-
-    public NumberHoldrer(int anInt, float aFloat) {
-        this.anInt = anInt;
-        this.aFloat = aFloat;
-    }
-
-    public int getAnInt() {
-        return anInt;
-    }
-
-    public float getaFloat() {
-        return aFloat;
-    }
 }
--- src/main/java/JavaBasicPart2/Task4/Task4.java
+++ src/main/java/JavaBasicPart2/Task4/Task4.java
@@ -3,6 +3,6 @@ package JavaBasicPart2.Task4;
 public class Task4 {
     public static void main(String[] args) {
         NumberHoldrer numberHoldrer = new NumberHoldrer(12,12.2f);
-        System.out.println(numberHoldrer.getaFloat()+" "+ numberHoldrer.getAnInt());
+        System.out.println(numberHoldrer.aFloat+" "+ numberHoldrer.anInt);
     }
 }
