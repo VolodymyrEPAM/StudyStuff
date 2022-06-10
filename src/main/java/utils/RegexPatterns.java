e9bb71c27780e45fc9f0e20f26411ac816424e5e
Volodymyr Tybolchuk <Volodymyr_Tybolchuk@epam.com>
2022-06-10 13:55:41 +0300

    fix applied

--- src/main/java/utils/RegexPatterns.java
+++ src/main/java/utils/RegexPatterns.java
@@ -13,33 +13,33 @@ public class RegexPatterns {
     private final static String countryPattern = "\\w{3}";
 
     public static String getStreetName(String address) {
-        String street = Arrays.asList(address.split("•")).get(0);
-        return getValueByRegex(street.trim(), streetNamePattern).trim();
+        String neededAddressPart = Arrays.asList(address.split("•")).get(0);
+        return getValueByRegex(neededAddressPart.trim(), streetNamePattern).trim();
     }
 
     public static String getStreetNumber(String address) {
-        String street = Arrays.asList(address.split("•")).get(0);
-        return getValueByRegex(street.trim(), streetNumberPattern).trim();
+        String neededAddressPart = Arrays.asList(address.split("•")).get(0);
+        return getValueByRegex(neededAddressPart.trim(), streetNumberPattern).trim();
     }
 
     public static String getSuiteNumber(String address) {
-        String street = Arrays.asList(address.split("•")).get(1);
-        return getValueByRegex(street.trim(), suiteNumberPattern).trim();
+        String neededAddressPart = Arrays.asList(address.split("•")).get(1);
+        return getValueByRegex(neededAddressPart.trim(), suiteNumberPattern).trim();
     }
 
     public static String getStateName(String address) {
-        String street = Arrays.asList(address.split(",")).get(1);
-        return getValueByRegex(street.trim(), statePattern).trim();
+        String neededAddressPart = Arrays.asList(address.split(",")).get(1);
+        return getValueByRegex(neededAddressPart.trim(), statePattern).trim();
     }
 
     public static String getPostalCode(String address) {
-        String street = Arrays.asList(address.split("•")).get(1);
-        return getValueByRegex(street.trim(), postalCodePattern).trim();
+        String neededAddressPart = Arrays.asList(address.split("•")).get(1);
+        return getValueByRegex(neededAddressPart.trim(), postalCodePattern).trim();
     }
 
     public static String getCountry(String address) {
-        String street = Arrays.asList(address.split("•")).get(2);
-        return getValueByRegex(street.trim(), countryPattern).trim();
+        String neededAddressPart = Arrays.asList(address.split("•")).get(2);
+        return getValueByRegex(neededAddressPart.trim(), countryPattern).trim();
     }
 
     private static Boolean isRegex(String expression, String regex) {
